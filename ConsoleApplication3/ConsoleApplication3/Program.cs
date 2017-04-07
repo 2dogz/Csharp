e745f7ad5f8dd7f519e5f764b59dffba6a0cd1ed
2dogz <kylewebb58@yahoo.com>
2017-04-06 19:55:09 -0500

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    Console.WriteLine("Go");
+                }
+
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("Cards!");
+                }
+              
+                else if (i % 4 == 0 & i % 7 == 0)
+                {
+                    Console.WriteLine("Go Cards!");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                    Console.ReadLine();
+                }
+
+            }
+
+        }
+    }
+}
