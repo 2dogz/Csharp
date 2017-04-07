63a3c6992b96e573f65a094e97d7923389115ffd
2dogz <kylewebb58@yahoo.com>
2017-04-06 19:52:18 -0500

    Add files via upload

---
+++ ConsoleReadInvitations/ConsoleReadInvitations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleReadInvitations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textFile = new TextFile("Invitations.txt");
+            var linesRead = textFile.LinesRead;
+
+            foreach (string line in linesRead) 
+            {
+                //format salutation as:
+                //
+                //  Dear Ms. Hillary Clinton:
+                //      ~or~
+                //  Dear Mr. Ted Cruz:
+                string[] lineElements = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                string salutation = lineElements[2] == "M" ? "Dear Mr. " : "Dear Ms. ";
+                string firstName = lineElements[1];
+                string lastName = lineElements[0];
+                Console.WriteLine(salutation + firstName + " " + lastName.Substring(0, lastName.Length - 1) + ":");
+
+
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
---
+++ ConsoleReadInvitations/ConsoleReadInvitations/TextFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleReadInvitations
+{
+    class TextFile
+    {
+        public string InFile { get; set; }
+
+        public List<string> LinesRead { get; set; }
+
+        public TextFile(string inFile = "") 
+        {
+            if (inFile.Length > 0)
+            {
+                this.InFile = inFile;
+                ReadFile();
+            }
+        }
+
+	    // Read in a file line-by-line, and store it all in a List.
+        private void ReadFile() 
+        {
+	        List<string> list = new List<string>();
+
+	        using (StreamReader reader = new StreamReader(this.InFile))
+	        {
+	            string line;
+	            while ((line = reader.ReadLine()) != null)
+		            list.Add(line); // Add to list.
+	        }
+
+            this.LinesRead = list;
+        }
+    }
+}
